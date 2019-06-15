08cecfa5950c67bc953d8b949a3b3707db70cd37
Ben <benjaminstevenburke@gmail.com>
2019-06-15 02:38:25 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,20 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
+var cardOne = [0];
+cardsInPlay.push(cards[cardOne]);
+console.log("the user flipped " + cardsInPlay);
+var cardTwo= [1];
+cardsInPlay.push(cards[cardTwo]);
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log("the user flipped" + cardsInPlay);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+if (cardsInPlay.length == 2) {
+	if (cardsInPlay[0] == cardsInPlay[1]) {
+		alert("You found a Match!");
+	}else{
+		alert("Sorry, try again.")
+	} 
+
+	}
+
